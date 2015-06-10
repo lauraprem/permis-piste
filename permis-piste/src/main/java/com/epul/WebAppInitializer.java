be8e5a96a0491cae9f67d5441c36e8fce95366a6
Laura <premillieu.laura@gmail.com>
2015-06-10 17:42:56 +0200

    configuration connexion bdd suppression partie client

--- permis-piste/src/main/java/com/epul/WebAppInitializer.java
+++ permis-piste/src/main/java/com/epul/WebAppInitializer.java
@@ -4,7 +4,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.XmlWebApplicationContext;
@@ -29,8 +28,8 @@ public class WebAppInitializer implements WebApplicationInitializer {
         servletContext.addListener(new ContextLoaderListener(appContext));
 
         //Database Console for managing the app's database (TODO : profile)
-        ServletRegistration.Dynamic h2Servlet = servletContext.addServlet("h2console", WebServlet.class);
-        h2Servlet.setLoadOnStartup(2);
-        h2Servlet.addMapping("/console/database/*");
+//        ServletRegistration.Dynamic h2Servlet = servletContext.addServlet("h2console", WebServlet.class);
+//        h2Servlet.setLoadOnStartup(2);
+//        h2Servlet.addMapping("/console/database/*");
     }
 }
--- permis-piste/src/main/java/com/epul/repository/SampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epul.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.epul.model.Sample;
-
-public interface SampleRepository extends JpaRepository<Sample, Long> {
-
-}
