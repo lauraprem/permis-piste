360ad47fee9fb94738ff7e42ec0db5747fb5a901
Laura <premillieu.laura@gmail.com>
2015-06-27 04:15:20 +0200

    Obitent correction

--- src/main/java/com/epul/repository/IObtientDao.java
+++ src/main/java/com/epul/repository/IObtientDao.java
@@ -3,13 +3,10 @@ package com.epul.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.epul.model.Obtient;
-
-import javax.transaction.Transactional;
 
 /**
  * Created by Pierre on 26/06/2015.
@@ -23,7 +20,7 @@ public interface IObtientDao extends JpaRepository<Obtient, Integer> {
 	   @Query("FROM Obtient WHERE NUMACTION= :numaction ")
 	    public List<Obtient> getObtientFromAction(@Param("numaction") int numaction);
 	    
-	    @Query("FROM Obtient WHERE NUMAPRENANT= :numapprenant ")
+	    @Query("FROM Obtient WHERE NUMAPPRENANT= :numapprenant ")
 	    public List<Obtient> getObtientFromApprenant(@Param("numapprenant") int numapprenant);
 	    
 	    @Query("FROM Obtient WHERE IDJEU= :idJeu ")
